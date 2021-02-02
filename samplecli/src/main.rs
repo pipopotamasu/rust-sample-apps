941bc8aa756530e0b3a3c385a27eebb7bdcd3ebc
pipopotamasu <ec0156hx@gmail.com>
2021-02-02 09:17:56 +0900

    cmd line tool

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Clap;
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main () {
+    let opts = Opts::parse();
+
+    match opts.formula_file {
+        Some(file) => println!("File specified!: {}", file),
+        None => println!("Nofile specified."),
+    }
+
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
